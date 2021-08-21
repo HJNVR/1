83513b7522e8a9b7fb2b317c9005d18ed74f0709
Jing Huang <starkhj.huang@mail.utoronto.ca>
2021-08-21 23:04:01 +0800

    fib Q1

---
+++ Fibonacci Numbers/Fibonacci.py
@@ -0,0 +1,54 @@
+def fib(n):
+    if n<=1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+# Q1: Find the Minimum Number of Fibonacci Numbers Whose Sums is K
+
+## My Attempt : Success
+class Solution(object):
+    '''
+    Input: k = 7
+    Output: 2 
+    Explanation: The Fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, ... 
+    For k = 7 we can use 2 + 5 = 7.
+    >>> s = Solution()
+    >>> s.findMinFibonacciNumbers(7)
+    '''
+    def fib(self,n):
+        if 0<=n<=1:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
+    
+    def findmax(self,l,k):
+        # l is now reversed from large to small
+        for i in l:
+            if i <= k:
+                return i
+        
+    def findMinFibonacciNumbers(self, k):
+        """
+        :type k: int
+        :rtype: int
+        """
+        l = []
+        count = 0
+        while self.fib(count) < k:
+            l.append(self.fib(count))
+            count +=1
+        
+        l.reverse()
+        
+        fib_sum = 0
+        result = 0
+        k_copy = k
+        while fib_sum <= k:
+            fib_sum += self.findmax(l,k_copy)
+            k_copy = k_copy - self.findmax(l,k_copy)
+            result += 1
+            if fib_sum == k:
+                break
+        return result
+            
+            
+            
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Algorithm-Collections
+## LeetCode, Books
+
+## 1. Fibonacci Numbers
+### Q1. Find the Minimum Number of Fibonacci Numbers Whose Sums is K
+	Input: k = 7
+    	Output: 2 
+    	Explanation: The Fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, ... 
+    	For k = 7 we can use 2 + 5 = 7.
